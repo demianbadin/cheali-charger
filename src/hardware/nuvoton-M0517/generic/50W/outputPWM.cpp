f1533cbe23fb1245747249a2435657a50cccea80
demianbadin <demian.badin@gmail.com>
2019-06-06 08:54:57 -0300

    Changes on OutputPWM and imaxB6-pins according to Turnigy TQ4

--- src/hardware/nuvoton-M0517/generic/50W/outputPWM.cpp
+++ src/hardware/nuvoton-M0517/generic/50W/outputPWM.cpp
@@ -37,32 +37,34 @@ namespace outputPWM {
 //TODO: remove
 volatile uint32_t pwm_n;
 
-volatile uint32_t PWM_valueA = 0;
+//volatile uint32_t PWM_valueA = 0;
 volatile uint32_t PWM_valueB = 0;
-volatile uint32_t PWM_sumA = 0;
+//volatile uint32_t PWM_sumA = 0;
 volatile uint32_t PWM_sumB = 0;
 
 void setCMR() {
     //modulate the PWM - we modulate the PWM signal to get more precision.
     //(the PWM frequency stays at about 32kHz)
-    PWM_sumA+=PWM_valueA;
+//    PWM_sumA+=PWM_valueA;
     PWM_sumB+=PWM_valueB;
 
-    PWM_SET_CMR(PWMA, PWM_CH1, PWM_sumA/OUTPUT_PWM_PRECISION_FACTOR);
-    PWM_SET_CMR(PWMB, PWM_CH2, PWM_sumB/OUTPUT_PWM_PRECISION_FACTOR);
+    PWM_SET_CMR(PWMB, PWM_CH0, PWM_sumB/OUTPUT_PWM_PRECISION_FACTOR);
+    PWM_SET_CMR(PWMB, PWM_CH1, PWM_sumB/OUTPUT_PWM_PRECISION_FACTOR);
 
-    PWM_sumA%=OUTPUT_PWM_PRECISION_FACTOR;
+//    PWM_sumA%=OUTPUT_PWM_PRECISION_FACTOR;
     PWM_sumB%=OUTPUT_PWM_PRECISION_FACTOR;
 }
 
 void initialize(void)
 {
-    CLK_EnableModuleClock(PWM01_MODULE);
-    CLK_EnableModuleClock(PWM67_MODULE);
+//    CLK_EnableModuleClock(PWM01_MODULE);
+//    CLK_EnableModuleClock(PWM67_MODULE);
+    CLK_EnableModuleClock(PWM45_MODULE);
 
     /* Select PWM module clock source */
-    CLK_SetModuleClock(PWM01_MODULE, CLK_CLKSEL1_PWM01_S_HCLK, 0);
-    CLK_SetModuleClock(PWM67_MODULE, CLK_CLKSEL2_PWM67_S_HCLK, 0);
+//    CLK_SetModuleClock(PWM01_MODULE, CLK_CLKSEL1_PWM01_S_HCLK, 0);
+//    CLK_SetModuleClock(PWM67_MODULE, CLK_CLKSEL2_PWM67_S_HCLK, 0);
+    CLK_SetModuleClock(PWM45_MODULE, CLK_CLKSEL2_PWM45_S_HCLK, 0);
 
     /* Reset PWMA channel0~channel3 */
     SYS_ResetModule(PWM03_RST);
@@ -70,58 +72,58 @@ void initialize(void)
 
     /* set PWMA channel 1 output configuration */
     // 32kHz
-    PWM_ConfigOutputChannel(PWMA, PWM_CH1, 32000, 0);
-    PWM_ConfigOutputChannel(PWMB, PWM_CH2, 32000, 0);
+    PWM_ConfigOutputChannel(PWMB, PWM_CH0, 32000, 0);
+    PWM_ConfigOutputChannel(PWMB, PWM_CH1, 32000, 0);
 
     /* Enable PWM Output path for PWMA channel 1 */
-    PWM_EnableOutput(PWMA, 1<< PWM_CH1);
-    PWM_EnableOutput(PWMB, 1<< PWM_CH2);
+    PWM_EnableOutput(PWMB, 1<< PWM_CH0);
+    PWM_EnableOutput(PWMB, 1<< PWM_CH1);
 
     // Enable PWM channel 1 period interrupt
-    PWMA->PIER = PWM_PIER_PWMIE1_Msk;
-    NVIC_EnableIRQ(PWMA_IRQn);
-    NVIC_SetPriority(PWMA_IRQn, OUTPUT_PWM_IRQ_PRIORITY);
+    PWMB->PIER = PWM_PIER_PWMIE0_Msk;
+    NVIC_EnableIRQ(PWMB_IRQn);
+    NVIC_SetPriority(PWMB_IRQn, OUTPUT_PWM_IRQ_PRIORITY);
 
-    pwm_n = PWM_GET_CNR(PWMB, PWM_CH2);
-    pwm_n = PWM_GET_CNR(PWMA, PWM_CH1);
+    pwm_n = PWM_GET_CNR(PWMB, PWM_CH1);
+    pwm_n = PWM_GET_CNR(PWMB, PWM_CH0);
 
     //Hm.... this is done in PWM_ConfigOutputChannel but we want to be sure
-    PWM_SET_CNR(PWMA, PWM_CH1, OUTPUT_PWM_PERIOD);
-    PWM_SET_CNR(PWMB, PWM_CH2, OUTPUT_PWM_PERIOD);
+    PWM_SET_CNR(PWMB, PWM_CH0, OUTPUT_PWM_PERIOD);
+    PWM_SET_CNR(PWMB, PWM_CH1, OUTPUT_PWM_PERIOD);
 
     // Start
-    PWM_Start(PWMA, 1<< PWM_CH1);
-    PWM_Start(PWMB, 1<< PWM_CH2);
+    PWM_Start(PWMB, 1<< PWM_CH0);
+    PWM_Start(PWMB, 1<< PWM_CH1);
 }
 
 void setPWM(uint8_t pin, uint32_t value)
 {
     LogDebug("setPWM pin:", pin, "value:", value);
-    if(pin == 20) {
-        PWM_valueA = value;
-        SYS->P2_MFP |= SYS_MFP_P21_PWM1;
-    } else if(pin == 26) {
+    if(pin == 23) {
         PWM_valueB = value;
-        SYS->P2_MFP |= SYS_MFP_P26_PWM6;
+        SYS->P2_MFP |= SYS_MFP_P24_PWM4; //modificado DMB para nuevo pinout 20->23
+    } else if(pin == 25) {
+        PWM_valueB = value;
+        SYS->P2_MFP |= SYS_MFP_P25_PWM5; //modificado DMB para nuevo pinout 26->25
     }
 }
 
 void disablePWM(uint8_t pin)
 {
-    if(pin == 20) {
-        SYS->P2_MFP &= ~SYS_MFP_P21_PWM1;
-    } else if (pin == 26) {
-        SYS->P2_MFP &= ~SYS_MFP_P26_PWM6;
+    if(pin == 23) {
+        SYS->P2_MFP &= ~SYS_MFP_P24_PWM4; //modificado DMB para nuevo pinout 20->23
+    } else if (pin == 25) {
+        SYS->P2_MFP &= ~SYS_MFP_P25_PWM5; //modificado DMB para nuevo pinout 26->25
     }
 }
 
 } //namespace outputPWM
 
 extern "C" {
-void PWMA_IRQHandler(void)
+void PWMB_IRQHandler(void)
 {
     outputPWM::setCMR();
-    PWM_ClearPeriodIntFlag(PWMA, PWM_CH1);
+    PWM_ClearPeriodIntFlag(PWMB, PWM_CH0);
 }
 } //extern "C"
 
